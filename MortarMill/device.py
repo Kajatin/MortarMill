dac34aecd4d8c6d878340feecd6de644e6e3ee90
Roland Kajatin <roland.kajatin@gmail.com>
2020-03-09 17:08:09 +0100

    Support for depth frame aligned with colour image. Logging setup in main. Ready to be used accross modules.

--- MortarMill/device.py
+++ MortarMill/device.py
@@ -1,26 +1,27 @@
-import datetime
 import logging
 
 import pyrealsense2 as rs
 import numpy as np
 import cv2 as cv
 
-logging.basicConfig(filename=f'logs/vision.log',
-                    level=logging.DEBUG,
-                    filemode='w',
-                    format='%(asctime)s.%(msecs)05d %(module)s %(levelname)s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
-logger = logging.getLogger(__name__)
 
 class Device():
     """description of class"""
 
-    def __init__(self, device, save=False, load_path=None):
+    def __init__(self, device, save=False, load_path=None, nir=False, align=True):
+        if device is None:
+            pass # TODO: handle case where device is None
+
+        self.logger = logging.getLogger(__name__)
 
         self.device = device
         self.serial = self.device.get_info(rs.camera_info.serial_number)
         self.pipeline = rs.pipeline()
         self.config = rs.config()
+
+        self.align = rs.align(rs.stream(2)) if align and not nir else None
+
+        self.logger.info('Setting up device {}'.format(self.serial))
 
         if load_path:
             # read from file
@@ -32,6 +33,12 @@ class Device():
             self.config.enable_stream(rs.stream(1), 1280, 720, rs.format(1), 30)
             self.config.enable_stream(rs.stream(2), 1920, 1080, rs.format(6), 30)
 
+            if nir:
+                # if NIR inputs are needed, the depth stream needs to be disabled
+                self.config.disable_stream(rs.stream(1))
+                self.config.enable_stream(rs.stream(3), 1, 1280, 800, rs.format(9), 30)
+                self.config.enable_stream(rs.stream(3), 2, 1280, 800, rs.format(9), 30)
+
             if save:
                 # save to file
                 date = datetime.datetime.now().strftime("%d%m%Y%H%M%S")
@@ -40,6 +47,11 @@ class Device():
 
         self.depth_image = None
         self.color_image = None
+
+        for sensor in self.device.sensors:
+            if sensor.is_depth_sensor():
+                self.depth_scale = sensor.as_depth_sensor().get_depth_scale()
+                break
 
         # TODO: add logging
         #print('Sensors found for device {} ({}):'.format(device.get_info(rs.camera_info.name),device.get_info(rs.camera_info.serial_number)))
@@ -58,20 +70,32 @@ class Device():
 
     def getFrames(self):
         frames = self.pipeline.wait_for_frames()
+        # frames.size() -> prints the number of frames returned
+
+        if self.align is not None:
+            frames = self.align.process(frames)
+
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            return None
+        nir_left_frame = frames.get_infrared_frame(1)
+        nir_right_frame = frames.get_infrared_frame(2)
 
-        # print distance at (x,y)
-        self.x = 240
-        self.y = 320
-        dist = depth_frame.get_distance(self.x,self.y)
-        print(dist)
+        #if not depth_frame or not color_frame:
+        #    return None
 
         # Convert images to numpy arrays
         self.depth_image = np.asanyarray(depth_frame.get_data())
         self.color_image = np.asanyarray(color_frame.get_data())
+        #self.nir_left_image = np.asanyarray(nir_left_frame.get_data())
+        #self.nir_right_image = np.asanyarray(nir_right_frame.get_data())
+
+        # scale depth image to meters
+        self.depth_image = self.depth_image * self.depth_scale
+
+        # print distance at (x,y)
+        self.x = int(depth_frame.height/2) # row
+        self.y = int(depth_frame.width/2) # column
+        print(self.depth_image[self.x,self.y])
 
         return (self.depth_image, self.color_image)
 
@@ -81,14 +105,17 @@ class Device():
         #    return 27
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv.applyColorMap(cv.convertScaleAbs(self.depth_image, alpha=0.03), cv.COLORMAP_JET)
+        alpha = 10#255/np.abs(self.depth_image).max()
+        depth_colormap = cv.applyColorMap(cv.convertScaleAbs(self.depth_image, alpha=alpha), cv.COLORMAP_JET)
 
-        cv.drawMarker(self.color_image, (self.x,self.y), (0,0,255), cv.MARKER_CROSS,thickness=2)
-        cv.drawMarker(depth_colormap, (self.x,self.y), (0,0,255), cv.MARKER_CROSS,thickness=2)
+        # draw a marker at pixel where the distance is shown
+        cv.drawMarker(depth_colormap, (self.y,self.x), (0,0,255), cv.MARKER_CROSS,thickness=2)
 
         # Show images
-        cv.imshow('RealSense Depth', depth_colormap)
-        cv.imshow('RealSense Color', self.color_image)
+        cv.imshow(f'RealSense Depth {self.serial}', depth_colormap)
+        cv.imshow(f'RealSense Color {self.serial}', self.color_image)
+        #cv.imshow(f'NIR Left {self.serial}', self.nir_left_image)
+        #cv.imshow(f'NIR Right {self.serial}', self.nir_right_image)
         
         return cv.waitKey(1)
 
